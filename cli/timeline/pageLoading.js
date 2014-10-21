1bcf0522a0620a53a7e7720004d0931a088a2e8b
wangkemiao <wangkemiao@baidu.com>
2014-10-21 23:50:42 +0800

    remove useless files

--- cli/timeline/pageLoading.js
+++ cli/timeline/pageLoading.js
@@ -5,7 +5,7 @@
 
 exports.cli = {
     description: 'chrome的timeline数据分析，分析页面加载速度，读取文件名为TimelineRawData-*的文件',
-    options: ['file:', 'output:', 'startMark:'],
+    options: ['file:', 'output:', 'startMark:', 'debug'],
     main: function (args, opts) {
         require('../../lib/pageLoading').process(args, opts);
     }
